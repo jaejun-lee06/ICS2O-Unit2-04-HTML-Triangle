c018681ed1f6f08b331bba977dca748cc5fb1c59
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-27 21:18:56 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of trianlge.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (1 / 2) * base * height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
